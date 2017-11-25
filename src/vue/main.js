fa8e8f9e33a4b60890674bf30b78fd6c2945a4eb
 Andrey Smelov <xor29a@bk.ru>
2017-11-25 20:40:12 +0300

    Add vue-resource package

--- src/vue/main.js
+++ src/vue/main.js
@@ -1,8 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueResource from 'vue-resource'
 import App from './App'
 import router from './router/index'
+
+Vue.use(VueResource)
+// Vue.http.options.root = 'http://budget.xorik.ru/api/'
+Vue.http.options.root = '/api/'
 
 Vue.config.productionTip = false
 
