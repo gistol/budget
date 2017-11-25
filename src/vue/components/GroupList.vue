4470fbbc2559425709d4a0c95ecf54aea29a3580
 Andrey Smelov <xor29a@bk.ru>
2017-11-25 23:11:39 +0300

    Hide the ID column

--- src/vue/components/GroupList.vue
+++ src/vue/components/GroupList.vue
@@ -1,12 +1,10 @@
 <template lang="pug">
   table.table.table-striped(width="100%")
     thead
-      th Id
       th Title
       th.text-right Actions
     tbody
       tr(v-for="item in value")
-        td {{item.id}}
         td {{item.title}}
         td.text-right
           button.btn.btn-sm.btn-default(@click="modal(item)") Edit
