28d4ac161a07a7816179f86f669a016cc1bac4ea
 Andrey Smelov <xor29a@bk.ru>
2017-11-26 10:57:04 +0300

    Change structure for the vue components

--- src/vue/router/index.js
+++ src/vue/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/components/Home'
-import Groups from '@/components/Groups'
+import Home from '@/components/Item/Home'
+import Groups from '@/components/Group/Groups'
 
 Vue.use(Router)
 
