81381641c0c74d58c2b78845aee4fabce0728480
 Andrey Smelov <xor29a@bk.ru>
2017-12-01 22:50:21 +0300

    Component with font-awesome icons selector

---
+++ src/vue/components/IconSelect.vue
@@ -0,0 +1,44 @@
+<template lang="pug">
+  modal(title="Select icon" :footer="false" v-model="opened")
+    .icon-list
+      a.text-primary(v-for="icon in list" @click.prevent="select(icon)" href)
+        i.fa.fa-3x.fa-fw(:class="'fa-'+icon")
+</template>
+
+<script>
+  import faList from '../utils/fa-list'
+  import { Modal } from 'uiv'
+
+  export default {
+    props: ['value'],
+
+    data () {
+      return {
+        opened: false,
+        list: faList
+      }
+    },
+
+    components: {
+      modal: Modal
+    },
+
+    methods: {
+      open: function () {
+        this.opened = true
+      },
+
+      select: function (icon) {
+        this.opened = false
+        this.$emit('input', icon)
+      }
+    }
+  }
+</script>
+
+<style>
+  .icon-list > a {
+    display: inline-block;
+    margin: 5px;
+  }
+</style>
